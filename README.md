7347b700a5b96944a7951079012473dbe1b52ec0
ABOSHV <164420680+ABOSHV@users.noreply.github.com>
2024-04-27 02:00:19 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# system
+**SYSTEM BY SFSF AND SYS**
+
+
+# YOUR BOT PREFIX MUST BE IN FILE > database.json
+```json
+ 
+   {
+ "prefix": "."
+}
+   
+```
+
+# ADD UR ID OR UR FRIEND'S ID IN FILE CONGIG.JS UNTIL YOU CONTROL THE ORDERS OF THE OWNERS
+
+```json
+exports.owners = [""];
+```
---
+++ log.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+  serverId: '1120330474657677362', 
+  logChannels: {
+    messageDelete: '1120363380654477354',
+    messageEdit: '1120363380654477354',
+    roleAdd: '1157088458712563712' ,
+    roleRemove: '1157088458712563712' ,
+    roleCreate: '1157088458712563712',
+    roleDelete: '1157088458712563712',
+    channelCreate: '1120363380654477354',
+    channelDelete: '1120363380654477354',
+    memberJoin: '1120363380654477354',
+    memberLeave: '1120363380654477354',
+    imageDelete: '1120363380654477354',
+    membernick:'1120363380654477354',
+    memberMute: '1120363380654477354',
+    voice: '1120363380654477354',
+}
+};
